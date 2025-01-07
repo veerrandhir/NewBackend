4e42e140149e00712ea338606638500795126923
veerrandhir <veerrandhir3@gmail.com>
2025-01-07 13:28:34 +0530

    Postman setup message ok stauts running DB connected

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,12 +3,12 @@ import { asyncHandler } from "../utils/asyncHandler.js"; // imported asyncHandle
 
 const registerUser = asyncHandler( async (req , res) =>{
     res.status(200).json({
-        message: "ok"
+        message: "ok Db is Showing message from controller"
     })
 })
 
 
 export {
-    registerUser,
+    registerUser, 
 
 }
